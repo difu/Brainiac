b96c48e40df5709c4212f262fdfc48121af59720
Dirk Fuchs <difu@mac.com>
2011-06-18 21:04:47 +0200

    docu

--- core/agent/channel.cpp
+++ core/agent/channel.cpp
@@ -1,5 +1,8 @@
 #include "channel.h"
 
+/** \brief Constructor
+        Init the channel with a max, min and a initial value
+**/
 Channel::Channel(qreal min, qreal max, qreal value)
 {
     m_max=max;
@@ -14,25 +17,48 @@ void Channel::advance()
     m_oldValue=m_value;
 }
 
+/** \brief inits channel
+        This slot is invoked if this channel has a parent
+        @sa Channel::setInherited()
+**/
 void Channel::changeValue(qreal value)
 {
     setValue(value);
 }
 
-qreal Channel::getMax()
+/** \brief returns the max value of this channel
+          note that this value may change from the min value of a fuzzy node!
+          @sa FuzzyBase::getMaxValue()
+**/
+qreal Channel::getMaxValue()
 {
     return m_max;
 }
 
-qreal Channel::getMin()
+/** \brief returns the min value of this channel
+        note that this value may change from the min value of a fuzzy node!
+        @sa FuzzyBase::getMinValue()
+**/
+qreal Channel::getMinValue()
 {
     return m_min;
 }
 
+/** \brief @returns the range between min and max
+**/
+qreal Channel::getRange()
+{
+    return qAbs(m_max-m_min);
+}
+
+/** \brief @returns the current value of this channel
+**/
 qreal Channel::getValue() {
     return m_value;
 }
 
+/** \brief @returns the old value of this channel
+**/
 qreal Channel::getOldValue() {
     return m_oldValue;
 }
@@ -55,6 +81,14 @@ void Channel::init(qreal value)
     emit valueChanged(value);
 }
 
+/** \brief makes this channel inherit its parent´s value
+
+                a channel´s value is automatically changing its value when the parent´s value changes
+
+                @param parent this channel´s parent
+                @param inherited true, if this channel should inherit
+
+**/
 void Channel::setInherited(Channel *parent, bool inherited)
 {
     m_inherited=inherited;
--- core/agent/channel.h
+++ core/agent/channel.h
@@ -3,15 +3,24 @@
 
 #include<QtCore>
 #include<QObject>
+#include"core/brainiacglobals.h"
 
+/** \brief  An agent´s channel
+
+        A channel may be an input or output source for the agent´s brain
+        @sa Brain
+        @sa Agent
+
+**/
 class Channel: public QObject
 {
     Q_OBJECT
 public:
-    Channel(qreal min=-std::numeric_limits<qreal>::max(), qreal max=std::numeric_limits<qreal>::max(), qreal value=0);
+    Channel(qreal min=BrainiacGlobals::MINQREAL, qreal max=BrainiacGlobals::MAXQREAL, qreal value=0);
     void advance();
-    qreal getMax();
-    qreal getMin();
+    qreal getMaxValue();
+    qreal getMinValue();
+    qreal getRange();
     void init(qreal value);
     bool isInherited();
     qreal getValue();
@@ -20,17 +29,17 @@ public:
     void setInherited(Channel *parent, bool inherited);
 
 public slots:
-    void changeValue(qreal value);
+    void changeValue(qreal value); //!< Slot to connect for channel inheritance  @sa Channel::setInherited()
 
 protected:
-    qreal m_value;
-    bool m_inherited;
-    qreal m_oldValue;
-    qreal m_max;
-    qreal m_min;
-    QString m_name;
+    qreal m_value; //!< Current value of this channel
+    bool m_inherited; //!< true, if this channel is inherited
+    qreal m_oldValue; //!< the old value of the channel
+    qreal m_max; //!< the max value
+    qreal m_min; //!< the min value
+    QString m_name; //!< this channel´s name
 signals:
-    void valueChanged(qreal value);
+    void valueChanged(qreal value); //!< emitted when the value of this channel changes @sa Channel::setInherited()
 };
 
 #endif // CHANNEL_H
