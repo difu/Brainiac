547c273a7fd141782610a26351fafd46ba0591e7
Dirk Fuchs <difu@mac.com>
2014-03-06 22:22:06 +0100

    Spline drawing

--- gui/Animation/motiontreeeditoritemconnector.cpp
+++ gui/Animation/motiontreeeditoritemconnector.cpp
@@ -7,26 +7,37 @@ MotionTreeEditorItemConnector::MotionTreeEditorItemConnector(EditorItem *startIt
 
 void MotionTreeEditorItemConnector::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
     if (m_StartItem->collidesWithItem(m_EndItem))
         return;
 
-    painter->setBrush(QColor(122, 163, 39));
+    static qreal _lineWidth=2.0;
     QLineF line(mapFromItem(m_StartItem, EditorItem::relxPos/2, EditorItem::relyPos/2), mapFromItem(m_EndItem, EditorItem::relxPos/2, EditorItem::relyPos/2));
     setLine(line);
     qreal angle = line.angle();
     angle+=20.0f;
     line.setAngle(angle);
     line.setLength(line.length()/2.0f);
-    painter->drawLine(line);
-    QLineF line2(line.p2(), mapFromItem(m_EndItem, EditorItem::relxPos/2, EditorItem::relyPos/2));
-    painter->drawLine(line2);
+
+
+    QPainterPath path;
+    path.moveTo(mapFromItem(m_StartItem, EditorItem::relxPos/2, EditorItem::relyPos/2));
+    path.cubicTo(line.p2(),line.p2(),mapFromItem(m_EndItem, EditorItem::relxPos/2, EditorItem::relyPos/2) );
+
+    painter->setPen(QPen(QColor(79, 106, 25), _lineWidth, Qt::SolidLine,
+                        Qt::FlatCap, Qt::MiterJoin));
+    painter->setBrush(QBrush(Qt::transparent));
+
+    painter->drawPath(path);
+
     if(m_progress>0.0 && m_progress <=1.0) {
+        qreal _offset=0.0;
+        qreal _progess=_offset+m_progress*(1.0-2*_offset);
         QPointF p;
-        if(m_progress<0.5) {
-            p=line.pointAt(m_progress*2.0);
-        } else {
-            p=line2.pointAt((m_progress-0.5)*2.0);
-        }
+        p=path.pointAtPercent(_progess);
+        painter->setBrush(Qt::lightGray);
+
         painter->drawEllipse(p,10,10);
     }
 
